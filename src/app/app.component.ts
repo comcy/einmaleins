bf841b860cf037638ab7d45d67b2cf76be009913
com_cy <christian.silfang@gmail.com>
2020-05-31 20:50:14 +0200

    added sample files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as exercises from '../data/exercises.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'einmaleins';
+  exercise = '';
+
+  ngOnInit() {
+  
+    const exercisePart = exercises.[1];
+
+    this.exercise = exercisePart[Math.floor(Math.random() * exercisePart.length)];
+
+  }
+
 }
